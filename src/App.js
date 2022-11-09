db4ca9f8ffde5e4aec86ead4ea59e025d91ffed3
Lautaro Nievas <lautaron@codojo.io>
2022-11-09 09:38:57 -0300

    remove formulario

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
-import SignInForm from './components/SignInForm';
 
 function App() {
   const [user, setUser] = useState({})
@@ -42,9 +41,7 @@ function App() {
         </div>
         <div className="col-8 container-form d-flex justify-content-center align-items-center">
           <div className='zone-form'>
-            <h2 className='mb-3'>Sign to dribble.</h2>
             <div id='signInDiv' className='mb-4'></div>
-            <SignInForm />
           </div>
         </div>
       </div>
