812f5ab1430450a72ca181ed78f3e56b7e5d0d7e
Lautaro Nievas <lautaron@codojo.io>
2022-11-10 10:09:53 -0300

    update

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
-import awsmobile from './aws-exports';
+import awsExports from './aws-exports';
 import { ThemeProvider } from '@aws-amplify/ui-react';
 
-Amplify.configure(awsmobile);
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
