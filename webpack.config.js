3021ecf7b3dbcbf9f922519f4b0d008ab1716e59
Lautaro Nievas <lautaron@codojo.io>
2022-11-14 15:37:57 -0300

    webpack add

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
