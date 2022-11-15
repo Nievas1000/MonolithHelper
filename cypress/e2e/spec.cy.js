d9e96601c6b56d58930bcb855b7ce9bf7314b7d8
Lautaro Nievas <lautaron@codojo.io>
2022-11-15 13:07:08 -0300

    test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,9 @@
 describe('Cognito', function () {
-  beforeEach(function () {
 
-    // Programmatically login via Amazon Cognito API
+  it('shows onboarding', function () {
     cy.loginByCognitoApi(
       Cypress.env('cognito_username'),
       Cypress.env('cognito_password')
     )
-  })
-
-  it('shows onboarding', function () {
-    cy.contains('Codojo')
   })
 })
