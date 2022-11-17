e7dc1fbcb5a74ddbfd1b770983d1e7ea9eff4271
Lautaro Nievas <lautaron@codojo.io>
2022-11-17 08:50:44 -0300

    data aws update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -9,7 +9,7 @@ const awsmobile = {
 	aws_user_pools_id: 'us-east-1_McFYVji0V',
 	aws_user_pools_web_client_id: 'atkq5thji5t1te8pbrar8goj1',
 	oauth: {
-		domain: process.env.REACT_APP_DOMAIN,
+		domain: 'b2xtfeocin84-staging.auth.us-east-1.amazoncognito.com',
 		scope: [
 			'phone',
 			'email',
@@ -17,8 +17,8 @@ const awsmobile = {
 			'profile',
 			'aws.cognito.signin.user.admin',
 		],
-		redirectSignIn: process.env.REACT_APP_AWS_REDIRECT,
-		redirectSignOut: process.env.REACT_APP_AWS_REDIRECT,
+		redirectSignIn: 'https://d3k7je3o78czwo.cloudfront.net/',
+		redirectSignOut: 'https://d3k7je3o78czwo.cloudfront.net/',
 		responseType: 'code',
 	},
 	federationTarget: 'COGNITO_USER_POOLS',
