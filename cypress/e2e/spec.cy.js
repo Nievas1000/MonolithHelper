558bfbd261c21fe5dd741e6797d15a5795195db7
Lautaro Nievas <lautaron@codojo.io>
2022-11-18 14:23:42 -0300

    add test login again

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,11 @@
 describe('Cognito', function () {
 	// Programmatically login via Amazon Cognito API
-	/* it('Test login aws', function () {
+	it('Test login aws', function () {
 		cy.loginByCognitoApi(
 			Cypress.env('cognito_username'),
 			Cypress.env('cognito_password')
 		);
-	}); */
+	});
 });
 
 describe('Google', function () {
