0e18142e2ce637af6ecdaeda70dcd461b4a4ea40
Lautaro Nievas <lautaron@codojo.io>
2022-11-18 13:48:50 -0300

    update data aws

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,9 +6,8 @@ const awsmobile = {
 	aws_cognito_identity_pool_id:
 		process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
 	aws_cognito_region: process.env.REACT_APP_AWS_PROJECT_REGION,
-	aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
-	aws_user_pools_web_client_id:
-		process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
+	aws_user_pools_id: 'us-east-1_McFYVji0V',
+	aws_user_pools_web_client_id: 'atkq5thji5t1te8pbrar8goj1',
 	oauth: {
 		domain: process.env.REACT_APP_AWS_REDIRECT,
 		scope: [
